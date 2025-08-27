4dca3684223cdd8e04b97507f8495b20ad53c248
Ethantobot <156458071+Ethantobot11@users.noreply.github.com>
2025-08-28 01:51:20 +0200

    Update PlayVideoState.hx

--- source/PlayVideoState.hx
+++ source/PlayVideoState.hx
@@ -26,118 +26,115 @@ import sys.FileSystem;
 class PlayVideoState extends MusicBeatState
 {
 
-	public static var videoID:String = 'credits3';
+ public static var videoID:String = 'credits3';
 
-	override function create()
-	{
-		super.create();
+ override function create()
+ {
+  super.create();
 
-        startVideo(videoID);
-    }
+        startVideo(videoID);
+    }
 
-   function goToMenu(){
-        LoadingState.loadAndSwitchState(new AmongStoryMenuState(), true);
-		FlxG.sound.playMusic(Paths.music('freakyMenu'));
-   }
+   function goToMenu(){
+        LoadingState.loadAndSwitchState(new AmongStoryMenuState(), true);
+  FlxG.sound.playMusic(Paths.music('freakyMenu'));
+   }
 
-	override function update(elapsed:Float)
-	{
-		super.update(elapsed);
-	}
+ override function update(elapsed:Float)
+ {
+  super.update(elapsed);
+ }
 
 
-    		if(SScript.global.exists(scriptFile))
-				doPush = false;
+      if(SScript.global.exists(scriptFile))
+    doPush = false;
+  }
+  #end
+ }
 
-			if(doPush) initHScript(scriptFile);
-		}
-		#end
-	}
+ public function getLuaObject(tag:String, text:Bool=true):FlxSprite {
+  #if LUA_ALLOWED
+  if(modchartSprites.exists(tag)) return modchartSprites.get(tag);
+  if(text && modchartTexts.exists(tag)) return modchartTexts.get(tag);
+  if(variables.exists(tag)) return variables.get(tag);
+  #end
+  return null;
+ }
 
-	public function getLuaObject(tag:String, text:Bool=true):FlxSprite {
-		#if LUA_ALLOWED
-		if(modchartSprites.exists(tag)) return modchartSprites.get(tag);
-		if(text && modchartTexts.exists(tag)) return modchartTexts.get(tag);
-		if(variables.exists(tag)) return variables.get(tag);
-		#end
-		return null;
-	}
+ function startCharacterPos(char:Character, ?gfCheck:Bool = false) {
+  if(gfCheck && char.curCharacter.startsWith('gf')) { //IF DAD IS GIRLFRIEND, HE GOES TO HER POSITION
+   char.setPosition(GF_X, GF_Y);
+   char.scrollFactor.set(0.95, 0.95);
+   char.danceEveryNumBeats = 2;
+  }
+  char.x += char.positionArray[0];
+  char.y += char.positionArray[1];
+ }
 
-	function startCharacterPos(char:Character, ?gfCheck:Bool = false) {
-		if(gfCheck && char.curCharacter.startsWith('gf')) { //IF DAD IS GIRLFRIEND, HE GOES TO HER POSITION
-			char.setPosition(GF_X, GF_Y);
-			char.scrollFactor.set(0.95, 0.95);
-			char.danceEveryNumBeats = 2;
-		}
-		char.x += char.positionArray[0];
-		char.y += char.positionArray[1];
-	}
+ public var videoCutscene:VideoSprite = null;
+ public function startVideo(name:String, forMidSong:Bool = false, canSkip:Bool = true, loop:Bool = false, playOnLoad:Bool = true)
+ {
+  #if VIDEOS_ALLOWED
+  inCutscene = !forMidSong;
+  canPause = forMidSong;
 
-	public var videoCutscene:VideoSprite = null;
-	public function startVideo(name:String, forMidSong:Bool = false, canSkip:Bool = true, loop:Bool = false, playOnLoad:Bool = true)
-	{
-		#if VIDEOS_ALLOWED
-		inCutscene = !forMidSong;
-		canPause = forMidSong;
+  var foundFile:Bool = false;
+  var fileName:String = Paths.video(name);
 
-		var foundFile:Bool = false;
-		var fileName:String = Paths.video(name);
+  #if sys
+  if (FileSystem.exists(fileName))
+  #else
+  if (OpenFlAssets.exists(fileName))
+  #end
+  foundFile = true;
 
-		#if sys
-		if (FileSystem.exists(fileName))
-		#else
-		if (OpenFlAssets.exists(fileName))
-		#end
-		foundFile = true;
+  if (foundFile)
+  {
+   videoCutscene = new VideoSprite(fileName, forMidSong, canSkip, loop);
+   if(forMidSong) videoCutscene.videoSprite.bitmap.rate = playbackRate;
 
-		if (foundFile)
-		{
-			videoCutscene = new VideoSprite(fileName, forMidSong, canSkip, loop);
-			if(forMidSong) videoCutscene.videoSprite.bitmap.rate = playbackRate;
+   // Finish callback
+   if (!forMidSong)
+   {
+    function onVideoEnd()
+    {
+     if (!isDead && generatedMusic && PlayState.SONG.notes[Std.int(curStep / 16)] != null && !endingSong && !isCameraOnForcedPos)
+     {
+      moveCameraSection();
+      FlxG.camera.snapToTarget();
+     }
+     videoCutscene = null;
+     canPause = true;
+     inCutscene = false;
+     startAndEnd();
+    }
+    videoCutscene.finishCallback = onVideoEnd;
+    videoCutscene.onSkip = onVideoEnd;
+   }
+   if (GameOverSubstate.instance != null && isDead) GameOverSubstate.instance.add(videoCutscene);
+   else add(videoCutscene);
 
-			// Finish callback
-			if (!forMidSong)
-			{
-				function onVideoEnd()
-				{
-					if (!isDead && generatedMusic && PlayState.SONG.notes[Std.int(curStep / 16)] != null && !endingSong && !isCameraOnForcedPos)
-					{
-						moveCameraSection();
-						FlxG.camera.snapToTarget();
-					}
-					videoCutscene = null;
-					canPause = true;
-					inCutscene = false;
-					startAndEnd();
-				}
-				videoCutscene.finishCallback = onVideoEnd;
-				videoCutscene.onSkip = onVideoEnd;
-			}
-			if (GameOverSubstate.instance != null && isDead) GameOverSubstate.instance.add(videoCutscene);
-			else add(videoCutscene);
+   if (playOnLoad)
+    videoCutscene.play();
+   return videoCutscene;
+  }
+  #if LUA_ALLOWED
+  else addTextToDebug("Video not found: " + fileName, FlxColor.RED);
+  #else
+  else FlxG.log.error("Video not found: " + fileName);
+  #end
+  #else
+  FlxG.log.warn('Platform not supported!');
+  startAndEnd();
+  #end
+  return null;
+ } 
 
-			if (playOnLoad)
-				videoCutscene.play();
-			return videoCutscene;
-		}
-		#if LUA_ALLOWED
-		else addTextToDebug("Video not found: " + fileName, FlxColor.RED);
-		#else
-		else FlxG.log.error("Video not found: " + fileName);
-		#end
-		#else
-		FlxG.log.warn('Platform not supported!');
-		startAndEnd();
-		#end
-		return null;
-	}	
-
-		function startAndEnd()
-	    {
-		   if(endingSong)
-			   endSong();
-		   else
-			   startCountdown();
-	     }
-    }
-}
+  function startAndEnd()
+     {
+     if(endingSong)
+      endSong();
+     else
+      startCountdown();
+      }
+    }
